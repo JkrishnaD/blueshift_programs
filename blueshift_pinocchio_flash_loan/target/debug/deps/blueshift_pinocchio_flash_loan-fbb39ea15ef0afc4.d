a354ba01b1ef5b34d2c1e248e11f5a916180a5d4
JayaKrishna <jayak5063@gmail.com>
2025-08-30 13:41:30 +0530

    Delete target directory

--- blueshift_pinocchio_flash_loan/target/debug/deps/blueshift_pinocchio_flash_loan-fbb39ea15ef0afc4.d
+++
@@ -1,9 +0,0 @@
-/Users/jayakrishna/Documents/blueshift/blueshift_pinocchio_flash_loan/target/debug/deps/libblueshift_pinocchio_flash_loan-fbb39ea15ef0afc4.rmeta: src/lib.rs src/instructions/mod.rs src/instructions/helpers.rs src/instructions/loan.rs src/instructions/repay.rs
-
-/Users/jayakrishna/Documents/blueshift/blueshift_pinocchio_flash_loan/target/debug/deps/blueshift_pinocchio_flash_loan-fbb39ea15ef0afc4.d: src/lib.rs src/instructions/mod.rs src/instructions/helpers.rs src/instructions/loan.rs src/instructions/repay.rs
-
-src/lib.rs:
-src/instructions/mod.rs:
-src/instructions/helpers.rs:
-src/instructions/loan.rs:
-src/instructions/repay.rs:
--- blueshift_pinocchio_flash_loan/target/debug/deps/five8_core-5c5ded42c6d06d4f.d
+++
@@ -1,6 +0,0 @@
-/Users/jayakrishna/Documents/blueshift/blueshift_pinocchio_flash_loan/target/debug/deps/libfive8_core-5c5ded42c6d06d4f.rmeta: /Users/jayakrishna/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/five8_core-0.1.2/src/lib.rs /Users/jayakrishna/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/five8_core-0.1.2/src/../README.md
-
-/Users/jayakrishna/Documents/blueshift/blueshift_pinocchio_flash_loan/target/debug/deps/five8_core-5c5ded42c6d06d4f.d: /Users/jayakrishna/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/five8_core-0.1.2/src/lib.rs /Users/jayakrishna/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/five8_core-0.1.2/src/../README.md
-
-/Users/jayakrishna/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/five8_core-0.1.2/src/lib.rs:
-/Users/jayakrishna/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/five8_core-0.1.2/src/../README.md:
