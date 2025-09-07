07af9cc15144e3c872a31202d5cb1b4db9082104
JKrishnaD <jayak5063@gmail.com>
2025-09-07 16:28:16 +0530

    updated state

--- blueshift_native_amm/src/state.rs
+++ blueshift_native_amm/src/state.rs
@@ -6,13 +6,15 @@ use pinocchio::{
 
 #[repr(C)]
 pub struct Config {
-    state: u8,
-    seed: [u8; 8],
     authority: Pubkey,
     mint_x: Pubkey,
     mint_y: Pubkey,
-    fee: [u8; 2],
-    bump: [u8; 1],
+    mint_x_vault: Pubkey,
+    mint_y_vault: Pubkey,
+    lp_mint: Pubkey,
+    fee: u16,
+    bump: u8,
+    lp_mint_bump: u8,
 }
 
 #[repr(u8)]
@@ -84,121 +86,27 @@ impl Config {
         &mut *(bytes.as_mut_ptr() as *mut Config)
     }
 
-    // getter methods to access the fields in the
-    #[inline(always)]
-    pub fn state(&self) -> u8 {
-        self.state
-    }
-
-    #[inline(always)]
-    pub fn seed(&self) -> u64 {
-        u64::from_le_bytes(self.seed)
-    }
-
-    #[inline(always)]
-    pub fn authority(&self) -> &Pubkey {
-        &self.authority
-    }
-
-    #[inline(always)]
-    pub fn mint_x(&self) -> &Pubkey {
-        &self.mint_x
-    }
-
-    #[inline(always)]
-    pub fn mint_y(&self) -> &Pubkey {
-        &self.mint_y
-    }
-
-    #[inline(always)]
-    pub fn fee(&self) -> u16 {
-        u16::from_le_bytes(self.fee)
-    }
-
-    #[inline(always)]
-    pub fn bump(&self) -> [u8; 1] {
-        self.bump
-    }
-
-    #[inline(always)]
-    pub fn set_state(&mut self, state: u8) -> Result<(), ProgramError> {
-        if state.ge(&(AmmState::WithdrawOnly as u8)) {
-            return Err(ProgramError::InvalidAccountData);
-        }
-        self.state = state as u8;
-        Ok(())
-    }
-
-    #[inline(always)]
-    pub fn set_seed(&mut self, seed: u64) -> Result<(), ProgramError> {
-        if seed == 0 {
-            return Err(ProgramError::InvalidAccountData);
-        }
-        self.seed = seed.to_le_bytes();
-        Ok(())
-    }
-
-    #[inline(always)]
-    pub fn set_fee(&mut self, fee: u16) -> Result<(), ProgramError> {
-        if fee.ge(&10_000) {
-            return Err(ProgramError::InvalidAccountData);
-        }
-        self.fee = fee.to_le_bytes();
-        Ok(())
-    }
-
-    #[inline(always)]
-    pub fn set_authority(&mut self, authority: Pubkey) -> Result<(), ProgramError> {
-        if authority == Pubkey::default() {
-            return Err(ProgramError::InvalidAccountData);
-        }
-        self.authority = authority;
-        Ok(())
-    }
-
-    #[inline(always)]
-    pub fn set_mint_x(&mut self, mint_x: Pubkey) -> Result<(), ProgramError> {
-        if mint_x == Pubkey::default() {
-            return Err(ProgramError::InvalidAccountData);
-        }
-        self.mint_x = mint_x;
-        Ok(())
-    }
-
-    #[inline(always)]
-    pub fn set_mint_y(&mut self, mint_y: Pubkey) -> Result<(), ProgramError> {
-        if mint_y == Pubkey::default() {
-            return Err(ProgramError::InvalidAccountData);
-        }
-        self.mint_y = mint_y;
-        Ok(())
-    }
-
-    #[inline(always)]
-    pub fn set_bump(&mut self, bump: u8) -> Result<(), ProgramError> {
-        if bump == 0 {
-            return Err(ProgramError::InvalidAccountData);
-        }
-        self.bump = bump.to_le_bytes();
-        Ok(())
-    }
-
     pub fn set_inner(
         &mut self,
-        seed: u64,
         authority: Pubkey,
         mint_x: Pubkey,
         mint_y: Pubkey,
+        mint_x_vault: Pubkey,
+        mint_y_vault: Pubkey,
+        lp_mint: Pubkey,
         fee: u16,
-        config_bump: [u8; 1],
+        bump: u8,
+        lp_mint_bump: u8,
     ) -> Result<(), ProgramError> {
-        self.set_state(AmmState::Initialized as u8)?;
-        self.set_seed(seed)?;
-        self.set_authority(authority)?;
-        self.set_mint_x(mint_x)?;
-        self.set_mint_y(mint_y)?;
-        self.set_fee(fee)?;
-        self.set_bump(config_bump[0])?;
+        self.authority = authority;
+        self.mint_x = mint_x;
+        self.mint_y = mint_y;
+        self.mint_x_vault = mint_x_vault;
+        self.mint_y_vault = mint_y_vault;
+        self.lp_mint = lp_mint;
+        self.fee = fee;
+        self.bump = bump;
+        self.lp_mint_bump = lp_mint_bump;
         Ok(())
     }
 
